8ad98413faa67bfe9de0e94893d9db265da89614
Mdsujansarkar <sujansarkar786@gmail.com>
2020-01-02 16:28:47 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+"use strict";
+
+
+$('.toggole').on('click',function(){
+$('.toggole-1').slideToggle();
+});
+$('.toggoles').on('click',function(){
+$('.toggole-2').slideToggle();
+});
+$('.search-icon').on('click',function(){
+$('.header-from').slideToggle();
+});
+$('.header-icon').on('click',function(){
+$('.test').slideToggle();
+});
+
+
+
+})(jQuery);	
